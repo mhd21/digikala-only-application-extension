df953766796ba97d8a39f460253ec342e9536eb0
mhd <dimitry2195@gmail.com>
2021-11-24 00:33:27 +0330

    added firefox

---
+++ firefox/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    (function () {
+
+        const observer = new MutationObserver( list => {
+            const evt = new CustomEvent('dom-changed', {detail: list});
+            document.body.dispatchEvent(evt)
+          });
+          observer.observe(document.body, {attributes: true, childList: true, subtree: true});
+          document.body.addEventListener('dom-changed', e => {
+              console.log('gggg');
+            const elements = document.getElementsByClassName('c-price__application-only-user');
+            if(elements.length > 0){
+                for(let i = 0; i < elements.length; i++) {
+                    elements[i].parentNode.removeChild(elements[i]);
+                }
+            }
+            
+          });
+        
+
+    
+      })();
+ }, false);
